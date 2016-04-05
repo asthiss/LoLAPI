c6c91b49c65dc8f1f55f2f57016c52c1ec0dd163
Petter Klang <petter.klang@inriver.com>
2016-04-05 23:05:06 +0200

    Champion mastery calls added

--- js/lolService.js
+++ js/lolService.js
@@ -1,12 +1,13 @@
 var lolService = {
     APIKEY: "b2f8a7fd-8688-47f5-9b16-1c4828661299",
+    BASEURL: "https://euw.api.pvp.net",
     getChampion: function(id) {
-        this.doRequest("https://euw.api.pvp.net/api/lol/euw/v1.2/champion" + id + "?api_key=" + this.APIKEY, function(data) {
+        this.doRequest("/api/lol/euw/v1.2/champion" + id + "?api_key=" + this.APIKEY, function(data) {
             console.log("getChampion success!");
         });
     },
     getChampions: function() {
-        this.doRequest("https://euw.api.pvp.net/api/lol/euw/v1.2/champion?api_key=" + this.APIKEY, function(data) {
+        this.doRequest("/api/lol/euw/v1.2/champion?api_key=" + this.APIKEY, function(data) {
             console.log("getChampions success!");
         });
     },
