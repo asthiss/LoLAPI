5df44bc06c46c9c6291c2c79f348d9ab8ab064bc
salmin89 <salmin89@hotmail.com>
2016-04-05 22:45:46 +0200

    test

--- js/app.js
+++ js/app.js
@@ -13,11 +13,7 @@ function lolApiUrl(name, callback) {
 }
 
 function ViewModel() {
-    var self = this;
-    self.apikey = ko.observable();
-    //self.user = ko.observable();
-    self.searchHistory = ko.observableArray([]);
-    self.summonerSearchQuery = ko.observable();
+
 
 
     (function init() {
