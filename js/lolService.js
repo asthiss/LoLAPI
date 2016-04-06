ec45a7fbdb83d270c1ab044c511fef121358f88c
Petter Klang <petter.klang@inriver.com>
2016-04-06 10:39:33 +0200

    asfd

--- js/lolService.js
+++ js/lolService.js
@@ -3,55 +3,72 @@ var lolService = {
     BASEURL: "https://euw.api.pvp.net",
     // Retrieve champion by ID.
     // data: ChampionListDto
-    getChampion: function(id) {
+    getChampion: function(id, callback) {
         this.doRequest("/api/lol/euw/v1.2/champion" + id + "?api_key=" + this.APIKEY, function(data) {
-            console.log("getChampion success!");
+            callback(data);
         });
     },
     // Retrieve all champions.
     // data: ChampionDto
-    getChampions: function() {
+    getChampions: function(callback) {
         this.doRequest("/api/lol/euw/v1.2/champion?api_key=" + this.APIKEY, function(data) {
-            console.log("getChampions success!");
+            callback(data);
         });
     },
     // Get a champion mastery by player id and champion id. Response code 204 means there were no masteries found for given player id or player id and champion id combination.
     // data: ChampionMasteryDTO
-    getChampionMastery: function(playerId, championId)
+    getChampionMastery: function(playerId, championId, callback)
     {
         this.doRequest("/championmastery/location/euw1/player/" + playerId + "/champion/" + championId + "?api_key=" + this.APIKEY, function(data)
         {
-            
+            callback(data);
         });
     },
     // Get all champion mastery entries sorted by number of champion points descending
     // data: List[ChampionMasteryDTO]
-    getChampionsMastery: function(playerId)
+    getChampionsMastery: function(playerId, callback)
     {
         this.doRequest("/championmastery/location/euw1/player/" + playerId + "/champions?api_key=" + this.APIKEY, function(data)
         {
-            
+            callback(data);
         });
     },
     // Get a player's total champion mastery score, which is sum of individual champion mastery levels
     // data: int
-    getChampionsMasteryScore: function(playerId)
+    getChampionsMasteryScore: function(playerId, callback)
     {
         this.doRequest("/championmastery/location/euw1/player/" + playerId + "/score?api_key=" + this.APIKEY, function(data)
         {
-            
+            callback(data);
         });
     },
     // Get specified number of top champion mastery entries sorted by number of champion points descending
     // data: List[ChampionMasteryDTO]
-    getChampionsMasteryTopChampions: function(playerId)
+    getChampionsMasteryTopChampions: function(playerId, callback)
     {
         this.doRequest("/championmastery/location/euw1/player/" + playerId + "/topchampions?api_key=" + this.APIKEY, function(data)
         {
-            
+            callback(data);
         });
     }, 
-
+    // Get current game information for the given summoner ID.
+    // data: CurrentGameInfo
+    getCurrentGame: function(summonerId, callback)
+    {
+        this.doRequest("/observer-mode/rest/consumer/getSpectatorGameInfo/euw1/" + summonerId + "?api_key=" + this.APIKEY, function(data)
+        {
+            callback(data);
+        });
+    },
+    // Get list of featured games.
+    // data: FeaturedGames
+    getFeaturedGame: function(callback)
+    {
+        this.doRequest("/observer-mode/rest/featured" + "?api_key=" + this.APIKEY, function(data)
+        {
+            callback(data);
+        });
+    },
     // Get summoner objects mapped by standardized summoner name for a given list of summoner names. (REST)
     // data: Map[string, SummonerDto]
     getSummoner: function(summonerName, callback)
